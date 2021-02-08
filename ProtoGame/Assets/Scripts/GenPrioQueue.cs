1bff9e2ed3429425a05dcbd671c7a92103457059
wllmwng1 <wwong1@ualberta.ca>
2021-02-08 14:48:29 -0700

    GenericPriorityQueue
    Added prio queue that accepts different objects.

---
+++ ProtoGame/Assets/Scripts/GenPrioQueue.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericPriorityQueue<T>
+{
+    private class GenericPriorityQueueNode
+    {
+        public int priority;
+        public T data;
+
+        public GenericPriorityQueueNode(int prio, T data)
+        {
+            this.priority = prio;
+            this.data = data;
+        }
+    }
+
+    private List<GenericPriorityQueueNode> heap = new List<GenericPriorityQueueNode>();
+
+    public T[] Queue
+    {
+        get
+        {
+            List<T> result = new List<T>();
+            foreach (GenericPriorityQueueNode node in heap)
+            {
+                result.Add(node.data);
+            }
+            return result.ToArray();
+        }
+    }
+
+    public void Insert(int prio, T data)
+    {
+        GenericPriorityQueueNode node = new GenericPriorityQueueNode(prio, data);
+        heap.Add(node);
+        this.sortUp(heap.Count - 1);
+    }
+
+    public T Pop()
+    {
+        T result = heap[0].data;
+        heap[0] = heap[heap.Count - 1];
+        heap.RemoveAt(heap.Count - 1);
+        sortDown(0);
+        return result;
+    }
+
+    public void Update(int i, int prio)
+    {
+        heap[i].priority = prio;
+        if (heap[(i - 1) / 2].priority > prio)
+        {
+            sortUp(i);
+        }
+        else
+        {
+            sortDown(i);
+        }
+    }
+
+    public void Update(T data, int prio)
+    {
+        int index = 0;
+        for (int i = 0; i < heap.Count; i++)
+        {
+            if (EqualityComparer<T>.Default.Equals(heap[i].data, data))
+            {
+                heap[i].priority = prio;
+                index = i;
+                break;
+            }
+        }
+        if (heap[(index - 1) / 2].priority > prio)
+        {
+            sortUp(index);
+        }
+        else
+        {
+            sortDown(index);
+        }
+    }
+
+    private void Swap(int i, int j)
+    {
+        GenericPriorityQueueNode tmp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = tmp;
+    }
+
+    private void sortUp(int i)
+    {
+        while ((i + 1) / 2 > 0)
+        {
+            if (heap[i].priority < heap[(i - 1) / 2].priority)
+            {
+                Swap(i, (i - 1) / 2);
+            }
+            i = (i - 1) / 2;
+        }
+    }
+
+    private void sortDown(int i)
+    {
+        while (i * 2 + 1 <= heap.Count)
+        {
+            int mc = minChild(i);
+            if (heap[i].priority > heap[mc].priority)
+            {
+                Swap(i, mc);
+            }
+            i = mc;
+        }
+    }
+
+    private int minChild(int i)
+    {
+        if (i * 2 + 2 > heap.Count - 1)
+        {
+            return i * 2 + 1;
+        }
+        else
+        {
+            if (heap[i * 2 + 1].priority < heap[i * 2 + 2].priority)
+            {
+                return i * 2 + 1;
+            }
+            else
+            {
+                return i * 2 + 2;
+            }
+        }
+    }
+}
+
---
+++ ProtoGame/Assets/Scripts/Tests/GenPriorityQueueTest.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GenPriorityQueueTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void InsertTest()
+        {
+            GenericPriorityQueue<string> queue = new GenericPriorityQueue<string>();
+            queue.Insert(5,"Fifth");
+            queue.Insert(3,"Third");
+            queue.Insert(1,"First");
+            queue.Insert(2,"Second");
+            queue.Insert(4,"Forth");
+            string[] expected = { "First", "Second", "Third", "Fifth", "Forth" };
+            Assert.AreEqual(queue.Queue, expected);
+        }
+
+        [Test]
+        public void PopTest()
+        {
+            GenericPriorityQueue<string> queue = new GenericPriorityQueue<string>();
+            queue.Insert(5, "Fifth");
+            queue.Insert(3, "Third");
+            queue.Insert(1, "First");
+            queue.Insert(2, "Second");
+            queue.Insert(4, "Forth");
+            string result = queue.Pop();
+            Assert.AreEqual(result, "First");
+            string[] expected = { "Second", "Forth", "Third", "Fifth"};
+            Assert.AreEqual(queue.Queue, expected);
+            result = queue.Pop();
+            Assert.AreEqual(result, "Second");
+            expected = new string[] { "Third", "Forth", "Fifth" };
+            Assert.AreEqual(queue.Queue, expected);
+        }
+
+        [Test]
+        public void UpdateIndexTest()
+        {
+            GenericPriorityQueue<string> queue = new GenericPriorityQueue<string>();
+            queue.Insert(1, "First");
+            queue.Insert(2, "Second");
+            queue.Insert(3, "Third");
+            queue.Insert(4, "Forth");
+            queue.Insert(5, "Fifth");
+            queue.Update(1, 6);
+            string[] expected = { "First", "Forth", "Third", "Second", "Fifth" };
+            Assert.AreEqual(queue.Queue, expected);
+            queue.Update(4, 0);
+            expected = new string[] { "Fifth", "First", "Third", "Second", "Forth" };
+            Assert.AreEqual(queue.Queue, expected);
+        }
+
+        [Test]
+        public void UpdateDataTest()
+        {
+            GenericPriorityQueue<string> queue = new GenericPriorityQueue<string>();
+            queue.Insert(1, "First");
+            queue.Insert(2, "Second");
+            queue.Insert(3, "Third");
+            queue.Insert(4, "Forth");
+            queue.Insert(5, "Fifth");
+            queue.Update("Second", 6);
+            string[] expected = { "First", "Forth", "Third", "Second", "Fifth" };
+            Assert.AreEqual(queue.Queue, expected);
+            queue.Update("Fifth", 0);
+            expected = new string[] { "Fifth", "First", "Third", "Second", "Forth" };
+            Assert.AreEqual(queue.Queue, expected);
+        }
+    }
+}
